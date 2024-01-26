b48c7a61966e64a66324eeda57ed591d79c889a7
Tal Damari <taldamari643@gmail.com>
2024-01-26 11:12:19 +0200

    seconed file

--- tal.java
+++ tal.java
@@ -4,7 +4,7 @@ public class tal
     {
         for (int i=0; i<7; i++)
         {
-            System.out.println("My name is Tal");
+            System.out.println("this is the second file");
         }
     }
 }
