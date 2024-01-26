95d2bb3594d90fd0ed71c75ea737f0c876ad1277
Tal Damari <taldamari643@gmail.com>
2024-01-26 11:06:02 +0200

    seconed file

---
+++ tal.java
@@ -0,0 +1,10 @@
+public class tal
+{
+    public static void main(String [] args)
+    {
+        for (int i=0; i<7; i++)
+        {
+            System.out.println("My name is Tal");
+        }
+    }
+}
